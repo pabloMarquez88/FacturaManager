f2d87be09a680ef660035a671b8103e5e5e52927
map53733 <pablo.marquez88@gmail.com>
2018-01-11 12:04:36 -0300

    Se personaliza mas on roles

---
+++ FM-backend/src/main/java/com/facturamanager/security/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.facturamanager.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	// Authentication : User --> Roles
+	protected void configure(AuthenticationManagerBuilder auth)
+			throws Exception {
+		auth.inMemoryAuthentication().withUser("user1").password("secret1")
+				.roles("USER").and().withUser("admin1").password("secret1")
+				.roles("USER", "ADMIN");
+	}
+	// Authorization : Role -> Access
+	/**
+	 * USER TIENE ACCESO A STUDENTS PERO A NADA MAS,
+	 * ADMIN TIENE ACCESO A TODO.
+	 */
+	protected void configure(HttpSecurity http) throws Exception {
+		http.httpBasic().and().authorizeRequests().antMatchers("/students/**")
+				.hasRole("USER").antMatchers("/**").hasRole("ADMIN").and()
+				.csrf().disable().headers().frameOptions().disable();
+	}
+}
